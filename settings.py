d73a1fa445009cd006a4450f571a8db4137396ce
Michael Bukachi <michaelbukachi@gmail.com>
2019-11-08 19:54:58 +0300

    Update gunicorn settings

--- settings.py
+++ settings.py
@@ -1,15 +1,16 @@
 import gunicorn
+import os
 from gevent import monkey
+
+PORT = os.getenv('PORT', 8004)
 
 monkey.patch_all()
 
-bind = '0.0.0.0:8004'
+bind = f'0.0.0.0:{PORT}'
 worker_class = 'gevent'
 workers = 2
 loglevel = 'debug'
 keepalive = 10
 timeout = 3600
 preload_app = True
-worker_tmp_dir = '/dev/shm'
 
-gunicorn.SERVER_SOFTWARE = 'Microsoft-IIS/6.0'
